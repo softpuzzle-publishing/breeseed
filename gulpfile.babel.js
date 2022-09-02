e00bb513ab1a633d8cf26c68e1b8d89e72e5fdcf
KIM HANA <hnone99@gmail.com>
2022-09-02 15:13:27 +0900

    no message

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,16 +6,17 @@ const babel = require('gulp-babel');
 const sourcemaps = require('gulp-sourcemaps');
 const fileinclude = require('gulp-file-include');
 const htmlbeautify = require('gulp-html-beautify');
-const htmllint = require('gulp-htmllint');
-const fancyLog = require('fancy-log');
-const colors = require('ansi-colors');
+const autoprefixer = require("gulp-autoprefixer");
+//const htmllint = require('gulp-htmllint');
+//const fancyLog = require('fancy-log');
+//const colors = require('ansi-colors');
 
 
 const src = 'src';
 const dist = 'dist';
 
 const paths = {
-    js: src + '/assets/js/**',
+    js: src + '/assets/js/**/*.js',
     css: src + '/assets/css/**',
     scss: src + '/assets/scss/**/*.scss',
     html : src + '/**/*.html',
@@ -48,7 +49,7 @@ function htmllintReporter(filepath, issues) {
 
 function html() {
     return gulp.src([paths.html])
-    .pipe(htmllint({}, htmllintReporter))
+    //.pipe(htmllint({}, htmllintReporter))
  		.pipe(fileinclude({
             prefix: '@@', //사용할땐 앞에@@ 를 붙이면됨
             basepath: '@file',
@@ -60,18 +61,20 @@ function html() {
 
 function js() {
     return gulp.src(paths.js) //개발코드 위치
-        //.pipe(babel()) // es6 -> es5로 컴파일
+        .pipe(babel()) // es6 -> es5로 컴파일
         .pipe(gulp.dest(dist + '/assets/js'))// dist에 복사
         .pipe(connect.reload());//변경되면 실시간 새로고침
 }
 
 function scss() {
-    return gulp.src(paths.scss) //개발코드 위치
-        .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) // 사스로 컴파일
-        .pipe(sourcemaps.write('./maps')) // 소스맵생성
-        .pipe(gulp.dest(dist + '/assets/css')) // dist에 복사
-        .pipe(connect.reload()); //변경되면 실시간 새로고침
+    return gulp
+      .src(paths.scss) //개발코드 위치
+      .pipe(sourcemaps.init())
+      .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError)) // 사스로 컴파일
+      .pipe(autoprefixer())
+      .pipe(sourcemaps.write("./maps")) // 소스맵생성
+      .pipe(gulp.dest(dist + "/assets/css")) // dist에 복사
+      .pipe(connect.reload()); //변경되면 실시간 새로고침
 }
 
 function lib() {
